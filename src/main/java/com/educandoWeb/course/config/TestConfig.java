526b4502d2abbf812ce81551a3ae2ff2b3410af4
AlcidesNeto32 <Alcidesnda@gmail.com>
2026-01-16 10:44:03 -0300

    feat: added Category entity,Repository and Service

--- src/main/java/com/educandoWeb/course/config/TestConfig.java
+++ src/main/java/com/educandoWeb/course/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.educandoWeb.course.config;
 
+import com.educandoWeb.course.entities.Category;
 import com.educandoWeb.course.entities.Order;
 import com.educandoWeb.course.entities.User;
 import com.educandoWeb.course.entities.enums.OrderStatus;
+import com.educandoWeb.course.repositories.CategoryRepository;
 import com.educandoWeb.course.repositories.OrderRepository;
 import com.educandoWeb.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,16 +22,26 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private UserRepository userRepository;
+
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
         User u1 = new User(null,"Maria Bronw","maria@gmail.com","9888888","123567");
         User u2 = new User(null,"José Yellow","jose@gmail.com","9834288","123567");
+
         Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1, OrderStatus.PAID);
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2,OrderStatus.WAITING_PAYMENT);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1,OrderStatus.DELIVERED);
+        categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
     }
--- src/main/java/com/educandoWeb/course/repositories/OrderRepository.java
+++ src/main/java/com/educandoWeb/course/repositories/OrderRepository.java
@@ -1,7 +1,6 @@
 package com.educandoWeb.course.repositories;
 
 import com.educandoWeb.course.entities.Order;
-import com.educandoWeb.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
