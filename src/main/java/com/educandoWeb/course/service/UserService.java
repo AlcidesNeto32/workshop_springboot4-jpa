c417d564f2197d431ae91301d880b572c8cac923
AlcidesNeto32 <Alcidesnda@gmail.com>
2026-02-24 10:49:39 -0300

    feat: exception handling  update

--- src/main/java/com/educandoWeb/course/service/UserService.java
+++ src/main/java/com/educandoWeb/course/service/UserService.java
@@ -43,9 +43,13 @@ public class UserService {
     }
 
     public User updateUser(Long id, User user) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, user);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, user);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
 
